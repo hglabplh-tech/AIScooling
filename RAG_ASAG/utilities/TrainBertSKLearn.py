3cc15dd6d83a4effe42b4ba8aad43096a8e067a0
Harald Glab-Plhak <harald.glab-plhak@t-online.de>
2026-05-01 01:39:43 +0200

    check in for saving work

--- RAG_ASAG/utilities/TrainBertSKLearn.py
+++ RAG_ASAG/utilities/TrainBertSKLearn.py
@@ -1,6 +1,13 @@
 import os
 import uuid
+from typing import Any
+import torch as to
 import pandas as pd
+from boto3.dynamodb.types import LIST
+from torch import Tensor
+from torch.optim._multi_tensor import AdamW
+from torch.utils.data import TensorDataset, DataLoader
+
 from RAG_ASAG.utilities.RAGUtils import load_vector_db, get_db_base_path, get_model_path
 from RAG_ASAG.utilities.RAGUtils import get_app_key
 from transformers import (
@@ -9,7 +16,7 @@ from transformers import (
     Trainer,
     TrainingArguments,
     BertModel, DataCollatorWithPadding,
-    BertTokenizer)
+    BertTokenizer, BertForSequenceClassification)
 
 import csv
 from sklearn.utils import Bunch
@@ -20,7 +27,7 @@ def create_BERTBase():
     # 1. Initialize tokenizer and model for regression (outputting a score)
     model_name = "bert-base-uncased"
     tokenizer = BertTokenizer.from_pretrained(model_name)
-    model = BertModel.from_pretrained(model_name, num_labels=4) # 1 for regression
+    model = BertForSequenceClassification.from_pretrained(model_name, num_labels=4) # 1 for regression
     text  = load_csv('temp_data')
     tokenizer.tokenize(text)
     tensors = DataCollatorWithPadding(tokenizer=tokenizer,  return_tensors='pt', padding=True)
@@ -41,19 +48,20 @@ def add_to_dataset(dataset_name, vector_db, query):
         csv_file.close()
 
 def load_csv(csv_name):
+    texts = []
     with open('../' + csv_name + '.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='\\')
         line_count = 0
-        text  = ""
         for row in csv_reader:
             line_count += 1
             for element in row:
-                text =  text + ' ' + str(line_count)  +  ' ' + element
-    return text
+                texts.append(str(line_count) + ':' + element)
+    return texts
 
 id  = uuid.uuid4().hex
 def load_dataset(dataset_name):
     suffix = '.csv'
+
     with open(r'../' + dataset_name + suffix) as csv_file:
         data_reader = csv.reader(csv_file, delimiter='\\')
         feature_names = next(data_reader)[:-1]
@@ -80,8 +88,7 @@ def get_all_data_from_db(vector_db,  query):
 
 
 def tokenize_function(tokenizer_bert, texts):
-    return tokenizer_bert.tokenize(texts, padding="max_length", truncation=True, max_length=128
-    )
+    return [tokenizer_bert(text, truncation=True, max_length=512, return_tensors='pt') for text in texts]
 
 
 ## TODO: work on this tomorrow 28.04.2026
@@ -92,32 +99,48 @@ def train_untrained_model(model_path):
     tokenizer_bert, model, _ = create_BERTBase()
     texts =load_csv('temp_data')
     model.train_batch_size  =  24
-    tokenized_datasets = tokenize_function(tokenizer_bert, texts)
-    mo
-    # 5. Define training parameters
-    training_args = TrainingArguments(
-        output_dir=model_path,
-        eval_strategy="epoch",
-        learning_rate=2e-5,
-        per_device_train_batch_size=16,
-        num_train_epochs=3,
-        weight_decay=0.01,
-    )
+    encoded = tokenize_function(tokenizer_bert, texts)
+    for encode in encoded:
+        labels = to.tensor([1])
+        print(encoded)
+        dataset = TensorDataset(encode['input_ids'], encode['attention_mask'], labels)
+        loader = DataLoader(dataset, batch_size=2, shuffle=True)
+        optimizer = AdamW(model.parameters(), lr=5e-5)
+        epochs = 5
+        train_model(loader, model, optimizer, epochs)
+    # Save your fine-tuned model
+    save_model(model, model_path)
 
-    # 6. Initialize the Trainer
-    trainer = Trainer(
-        eval_dataset=tokenized_datasets,
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_datasets
-    )
+
+def train_model(loader: DataLoader[tuple[Tensor, ...]], model: BertForSequenceClassification, optimizer, epochs):
+
 
     # 7. Start training
-    trainer.train()
-    print("end train -> save model")
-    # Save your fine-tuned model
-    model.save(model_path)
+    model.train()
+    for epoch in range(epochs):
+        for batch in loader:
+            # Move batch to GPU
+            b_input_ids, b_attn_mask, b_labels = [t.to('cpu') for t in batch]
 
+            # Clear previous gradients
+            model.zero_grad()
+
+            # Forward pass
+            outputs = model(b_input_ids, attention_mask=b_attn_mask, labels=b_labels)
+            loss = outputs.loss
+
+            # Backward pass
+            loss.backward()
+
+            # Update weights
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1} complete. Loss: {loss.item()}")
+    print("end train -> save model")
+
+
+def save_model(bert_model,  model_path):
+    to.save(bert_model, model_path)
 
 if  __name__ == '__main__':
     base_path = get_db_base_path()
@@ -127,5 +150,5 @@ if  __name__ == '__main__':
     while prompt !='exit':
         add_to_dataset('temp_data',vector_db, prompt)
         prompt = input("Prompt: ")
-    _, base_path =  get_model_path()
-    train_untrained_model(base_path)
+    model_path, base_path =  get_model_path()
+    train_untrained_model(model_path)
